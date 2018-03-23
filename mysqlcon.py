2e6506075e7b3b694ab4ced6563578dc1daa254b
Robert <25930094+robertojf2012@users.noreply.github.com>
2018-03-23 00:18:39 -0700

    Delete mysqlcon.py

--- mysqlcon.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-cnx = mysql.connector.connect(user='root', password='1234', host='127.0.0.1', database='login')
-query = ("SELECT * FROM usuarios")
-cursor = cnx.cursor()
-cursor.execute(query)
-cnx.close()
